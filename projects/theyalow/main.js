2890af61a434a375f417e178d72afc5f4fe1e6d5
SkynerSany <skynerstudent@gmail.com>
2019-12-02 15:06:42 +0300

    fix: changed theyalows style

--- projects/theyalow/main.js
+++ projects/theyalow/main.js
@@ -7,7 +7,10 @@ const windowSize = {
 };
 
 if (!window.matchMedia("(min-width: 376px)").matches) {
-    document.querySelector('link').href = 'assets/css/styleMobile.css';
+    document.querySelector('.wrapper').style.width = '170.7vw';
+    document.querySelector('.mobile-top-video').style.width = '640px';
+    document.querySelector('.navigation').style.width = '640px';
+    document.querySelector('.container').style.maxWidth = '640px';
     btnMobile.style.display = 'none';
 }
 
