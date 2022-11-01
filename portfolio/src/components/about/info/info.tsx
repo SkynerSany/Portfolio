28a3d56e4d0fda5bd54223e0a9ee73add030c036
Alexandr Rylkov <skynerstudent@gmail.com>
2022-11-01 22:04:46 +0300

    refactor: change text about me

--- portfolio/src/components/about/info/info.tsx
+++ portfolio/src/components/about/info/info.tsx
@@ -22,15 +22,20 @@ export default function Info() {
         <div className="about__textBox">
           <h2 className="about__title">about me</h2>
           <p className="about__text">Hello again everyone! So, you already know that my name is Alexandr. 
-              A little about myself: i just returned from the army, 24 y.o., allways in search new knowledge, 
-              I love computer sience childhood, I live in Krugloe, Belarus. Why programming? Everything is elementary - 
-              I like it, the profession of the future. I like watching result of my work and how people use it. 
+              A little about myself: 24 y.o., I just served in the army, live in Krugloe, Belarus. 
+              I love computer sience childhood, allways in search new knowledge. 
+          </p>
+          <p className="about__text">Why programming? Everything is elementary - 
+              I like to see the result of my work and the fact that people use them.  
               At the moment I specialize in front-end development. My skill stack include technology such as React, 
-              TypeScript and just JavaScript. Why should you choose me? I approach each order with great responsibility 
-              and love, as I want to make a name for myself, exclude plagiarism and negligence, work for quality, 
-              trying to make an order as quickly and uniquely as possible, taking into account all the edits and wishes. 
+              TypeScript and just JavaScript.
+          </p>
+          <p className="about__text">Why should you choose me? I approach each order with great responsibility, 
+              as I want to make a name for myself, exclude plagiarism and negligence, work for quality, 
+              trying to make an order as quickly and uniquely as possible, taking into account all the edits and wishes of client. 
               By trusting me, you will get the maximum return for your project, save your nerves and time. 
-              If you are interested in me , you want to know something more or use my services, then 
+          </p>
+          <p className="about__text">If you are interested in me , you want to know something more or use my services, then 
               I will provide all my contacts below.
           </p>
         </div>
